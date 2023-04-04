7b68aff8a4506a575c20531ea046d34bf0a0134b
Mayura Sandakalum <mssellapperuma@gmail.com>
2023-04-04 23:54:21 +0530

    changed

--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -24,7 +24,7 @@ const Main = (props) => {
   const currentPath = useLocation();
 
   const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
